804143da491d7a9c7804ba3d0dfd3bf46dd816d9
Massyl <massyl.haddad@gmail.com>
2022-10-30 12:07:37 +0100

    index hello fixs

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -5,8 +5,8 @@ const Hero = () => {
     <section className="hero-section">
       <h1 className="title">Hi I'm <span className="name">Massyl!</span></h1>
       <h3 className="description">I am Web Developer</h3>
-      <p className="summary">I am an aspiring web develeper passioned about everything web!</p>
-      <Button>Resume</Button>
+      <p className="summary">I am an aspiring Web Developer, passionate about everything web!</p>
+      <Button>Go to Resume</Button>
     </section>
   )
 }
