9af6f9e8bae8b3cf336f4ea39f3861970e73960f
MassylHaddad <massyl.haddad@gmail.com>
2022-12-07 20:08:44 +0100

    Update hero.tsx

--- sections/hero.tsx
+++ sections/hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section className="hero-section">
       <h1 className="title">Hi I'm <span className="name"> Massyl! </span></h1>
-      <h3 className="description">I am Web Developer</h3>
+      <h3 className="description">I am a Web Developer</h3>
       <p className="summary">I am an aspiring Web Developer, passionate about everything web!</p>
       <Button>See my Resume</Button>
     </section>
