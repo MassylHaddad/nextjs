b7291aa55119f1e3444f539166a064c9d92d6194
MassylHaddad <massyl.haddad@gmail.com>
2023-04-07 16:37:55 +0200

    Update resume.tsx

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>MyResume</title>
+        <title>Massyl's Resume</title>
         <meta name="Resume" content="Resume" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"
